29100b30ed0e125a82851d1586f9ee533b6dc340
Admin <sddlpeter@gmail.com>
2023-06-18 09:40:36 -0700

    upgrade spring-mybatis version to 3.0.2 and do some change

--- spring6-018-mybatis/src/test/java/com/powernode/spring6/test/SMTest.java
+++ spring6-018-mybatis/src/test/java/com/powernode/spring6/test/SMTest.java
@@ -8,8 +8,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SMTest {
     @Test
-    public void testSM() {
+    public void testSM(){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        // AccountMapper accountMapper = applicationContext.getBean("AccountMapper", AccountMapper.class);
+        AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+        try {
+            accountService.transfer("act-001", "act-002", 10000.0);
+            System.out.println("转账成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("转账失败");
+        }
     }
+
 }
